1c7506acf0ff0ab9fe93e54ccbc87485c5645a7e
dayvison06 <dayvison.rocha@hotmail.com>
2024-07-16 16:49:25 -0300

    Iniciando instalação do JWT

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,9 +16,12 @@ use App\Http\Controllers\Api\UserController;
 */
 
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Testando JWT
+Route::post('auth/login', [AuthController::class, 'login']);
 
 //First endpoint
 
